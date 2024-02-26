d40cbdd183f5291b3aa7bd137672064163b387f6
Jerren Dang <djerren@gmail.com>
2024-02-26 14:06:14 -0500

    added header outside of conditional

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -4,11 +4,13 @@ export const csrfFetch = async (url, options = {}) => {
     options.method = options.method || 'GET';
     options.headers = options.headers || {};
 
+    options.headers['Access-Control-Allow-Origin'] = 'https://muscle-metrics.onrender.com';
+
     if (options.method.toUpperCase() !== 'GET'){
         options.headers['Content-Type'] = 
             options.headers['Content-Type'] || 'application/json';
         options.headers['XSRF-Token'] = Cookies.get('XSRF-Token');
-        options.headers['Access-Control-Allow-Origin'] = "https://muscle-metrics.onrender.com"
+        // options.headers['Access-Control-Allow-Origin'] = "https://muscle-metrics.onrender.com"
     }
 
     url = process.env.NODE_ENV === 'production' ? 'https://activity-tracker-app-uty0.onrender.com' + url : 'http://localhost:8000' + url
